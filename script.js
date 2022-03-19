46137cb3c4dc17746dd26dea269cc060fc0f9135
FIRST_NAME LAST_NAME <khadijarejjaoui41@gmail.com>
2022-03-19 23:39:49 +0600

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let rating = 0;
-function clickedBtn(clicked) {
-  rating = document.getElementById(clicked).innerText;
-  return rating;
-}
 const submit = document.getElementById("submit");
 const text = document.getElementById("ratingInfo");
 const fs = document.getElementById("firstSection");
 const ss = document.getElementById("secondSection");
+function clickedBtn(clicked) {
+  rating = document.getElementById(clicked).innerText;
+  submit.disabled = false;
+  return rating;
+}
+if (rating === 0) {
+  submit.disabled = true;
+}
 submit.addEventListener("click", () => {
   text.innerText = `You selected ${rating} out of 5`;
   if (rating !== 0) {
